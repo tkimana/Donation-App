8ac081668fa33214cdb16504dd6fb5020c4acb9b
Theodore <kimana.theodore>
2021-03-26 15:45:38 -0500

    added

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 
 const express = require('express')
-const app= express()
+const mongoose = require('mongoose')
 require('dotenv').config()
 
+
+const app= express()
 app.get('/', (req, res)=>{
     res.send('hello from node')
 })
 
 
+// db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+.then(()=> console.log("DB conected"));
+
 const port = process.env.PORT || 8000
 
-
+// route
 app.listen(port, ()=>{
     console.log(`the server is running on port ${port}`)
 })
