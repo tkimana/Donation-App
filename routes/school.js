f7d15cb96ce81710627433cf60efb086c704409b
Theodore <kimana.theodore>
2021-04-20 15:36:33 -0500

    added some changes

--- routes/school.js
+++ routes/school.js
@@ -1,12 +1,13 @@
 const express= require("express")
 const router= express.Router()
-const {create, schoolById, read} = require("../controllers/school")
+const {create, schoolById, read, remove} = require("../controllers/school")
 const {requireSignin, isAuth, isAdmin}= require('../controllers/auth')
 const {userById}= require('../controllers/user')
 
-
-router.get("/school/create/:schoolId", read);
+// This router gets a single school information
+router.get("/school/:schoolId", read);
 router.post("/school/create/:userId", requireSignin, isAdmin, isAuth, create);
+router.delete("/school/:schoolId/userId", requireSignin, isAdmin, isAuth, remove)
 
 
 router.param("userId", userById);
