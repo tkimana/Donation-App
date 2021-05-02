7586eb28b02c39bbe8fe0ec41f2315758931a25d
Theodore <kimana.theodore>
2021-05-01 20:54:33 -0500

    added something

--- controllers/Principal.js
+++ controllers/Principal.js
@@ -1,6 +1,19 @@
 const Principal= require("../models/principal")
 const {errorHandler} = require('../helpers/dbErrorHandler')
 
+exports.principalById=(req, res, next, id)=>{
+    Principal.findById(id).exec((err, principal)=>{
+        if(err || !principal){
+            return res.status(400).json({
+                error:'Principal does not exist'
+            })
+        }
+
+      req.principal= principal
+      next()
+    })
+}
+
 // This creates new Principal and saves it in the database
 exports.create=(req, res)=>{
     const principal= new Principal(req.body)
@@ -11,5 +24,48 @@ exports.create=(req, res)=>{
             })
         }
         res.json({data})
+    })
+};
+
+exports.read=(req, res) => {
+    return res.json(res.principal);
+};
+
+exports.update=(req, res)=>{
+    const principal= req.principal
+    principal.name= req.body.name
+    principal.save((err, data)=>{
+        if(err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        res.json(data)
+    })
+}
+
+exports.remove=(req, res)=>{
+    const principal= req.principal
+    principal.name= req.body.name
+    principal.remove((err, data)=>{
+        if(err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        res.json({
+            message:"Principal deleted"
+        })
+    })
+}
+
+exports.list=(req, res)=>{
+    Principal.find().exec((err, data)=>{
+        if(err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        res.json(data)
     })
 }
--- routes/Principal.js
+++ routes/Principal.js
@@ -1,11 +1,18 @@
 const express= require("express")
 const router= express.Router()
-const {create} = require("../controllers/principal")
+const {create, principalById, read, remove, update, list} = require("../controllers/principal")
 const {requireSignin, isAuth, isAdmin}= require('../controllers/auth')
 const {userById}= require('../controllers/user')
 
+
+
+router.get('/principal/:principalId', read)
 router.post("/principal/create/:userId", requireSignin, isAdmin, isAuth, create);
+router.put("/principal/principalId/:userId", requireSignin, isAdmin, isAuth, update);
+router.delete("/principal/principalId/:userId", requireSignin, isAdmin, isAuth, remove);
+router.get('/principals', list)
 
 
+router.param('principalId', principalById);
 router.param("userId", userById);
 module.exports= router;
